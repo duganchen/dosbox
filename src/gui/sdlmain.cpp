065a4ddd1d20cde3e4f1ac852866ae7e271eb39e
Dugan Chen <thedoogster@gmail.com>
2015-12-28 15:54:56 -0800

    3.3 compatibility profile

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -1270,7 +1270,7 @@ dosurface:
 #endif
 #if SDL_VERSION_ATLEAST(2,0,0)
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3); 
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1); 
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3); 
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 
 		GFX_SetupWindowScaled(sdl.desktop.want_type);
