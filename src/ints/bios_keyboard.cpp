bbd02bf670faeeab704e1e234ebeaddf162428cf
Dugan Chen <thedoogster@gmail.com>
2016-01-02 12:01:40 -0800

    src/ints/bios_keyboard.py has no SDL1 support

--- src/ints/bios_keyboard.cpp
+++ src/ints/bios_keyboard.cpp
@@ -30,11 +30,7 @@
 /* SDL by default treats numlock and scrolllock different from all other keys.
  * In recent versions this can disabled by a environment variable which we set in sdlmain.cpp
  * Define the following if this is the case */
-#if SDL_VERSION_ATLEAST(1, 2, 14)
 #define CAN_USE_LOCK 1
-/* For lower versions of SDL we also use a slight hack to get the startup states of numclock and capslock right.
- * The proper way is in the mapper, but the repeating key is an unwanted side effect for lower versions of SDL */
-#endif
 
 static Bitu call_int16,call_irq1,call_irq6;
 
@@ -597,13 +593,6 @@ static void InitBiosSegment(void) {
 	mem_writew(BIOS_KEYBOARD_BUFFER_TAIL,0x1e);
 	Bit8u flag1 = 0;
 	Bit8u leds = 16; /* Ack received */
-
-#if SDL_VERSION_ATLEAST(1, 2, 14)
-//Nothing, mapper handles all.
-#else
-	if (startup_state_capslock) { flag1|=0x40; leds|=0x04;}
-	if (startup_state_numlock)  { flag1|=0x20; leds|=0x02;}
-#endif
 
 	mem_writeb(BIOS_KEYBOARD_FLAGS1,flag1);
 	mem_writeb(BIOS_KEYBOARD_FLAGS2,0);
