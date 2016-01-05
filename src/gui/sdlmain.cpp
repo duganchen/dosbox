1762a7d263f3335ea7b2c9a7b1f4ad4f18d765a0
Dugan Chen <thedoogster@gmail.com>
2016-01-04 22:22:37 -0800

    Texture coordinates are assumed to be normalized

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -990,7 +990,7 @@ dosurface:
 		glBindBuffer(GL_ARRAY_BUFFER, sdl.opengl.texture_vbo);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(sdl.opengl.texture_data), sdl.opengl.texture_data, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof (GLfloat), (GLvoid *)0);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_TRUE, 2 * sizeof (GLfloat), (GLvoid *)0);
 		glEnableVertexAttribArray(1);
 
 		sdl.desktop.type=SCREEN_OPENGL;
