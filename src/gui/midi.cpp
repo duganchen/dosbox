8a45ee2d5572b677fb972882672ec1bfd7eb86e2
Dugan Chen <thedoogster@gmail.com>
2016-01-21 18:52:21 -0800

    FluidSynth patch with wordexp support added

--- src/gui/midi.cpp
+++ src/gui/midi.cpp
@@ -70,6 +70,10 @@ MidiHandler Midi_none;
 
 /* Include different midi drivers, lowest ones get checked first for default */
 
+#ifdef C_FLUIDSYNTH
+#include "midi_fluidsynth.h"
+#endif
+
 #if defined(MACOSX)
 
 #include "midi_coremidi.h"
