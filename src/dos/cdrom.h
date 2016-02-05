db042a7bc7e321a2f4f2d851a8c6b12c13f79aa4
Dugan Chen <thedoogster@gmail.com>
2016-02-04 19:41:42 -0800

    SDL_Sound support removed

--- src/dos/cdrom.h
+++ src/dos/cdrom.h
@@ -35,10 +35,6 @@
 #include "../sdl_cdrom/compat_SDL_cdrom.h"
 #include "SDL_thread.h"
 
-#if defined(C_SDL_SOUND)
-#include "SDL_sound.h"
-#endif
-
 #define RAW_SECTOR_SIZE		2352
 #define COOKED_SECTOR_SIZE	2048
 
@@ -156,21 +152,6 @@ private:
 		BinaryFile();
 		std::ifstream *file;
 	};
-	
-	#if defined(C_SDL_SOUND)
-	class AudioFile : public TrackFile {
-	public:
-		AudioFile(const char *filename, bool &error);
-		~AudioFile();
-		bool read(Bit8u *buffer, int seek, int count);
-		int getLength();
-	private:
-		AudioFile();
-		Sound_Sample *sample;
-		int lastCount;
-		int lastSeek;
-	};
-	#endif
 	
 	struct Track {
 		int number;
