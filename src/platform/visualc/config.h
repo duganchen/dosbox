9ddea5141b7a46d478f60892f2fda78b9c467ff6
Henrik Nilsson <henrik.nilsson@bytequest.se>
2016-05-29 17:09:29 +0200

    Disable opengl support
    Not working in Virtualbox

--- src/platform/visualc/config.h
+++ src/platform/visualc/config.h
@@ -7,7 +7,7 @@
 #define C_SSHOT 0
 
 /* Define to 1 to use opengl display output support */
-#define C_OPENGL 1
+#define C_OPENGL 0
 
 /* Define to 1 to enable internal modem support, requires SDL_net */
 #define C_MODEM 1
